09b0b38ec69db42f8e10cfcc4421c34d110a3884
진상하 <98368934+slemdem@users.noreply.github.com>
2023-11-14 22:04:54 +0900

    Update mm.c

--- mm.c
+++ mm.c
@@ -1,14 +1,4 @@
-/*
- * mm-naive.c - The fastest, least memory-efficient malloc package.
- * 
- * In this naive approach, a block is allocated by simply incrementing
- * the brk pointer.  A block is pure payload. There are no headers or
- * footers.  Blocks are never coalesced or reused. Realloc is
- * implemented directly using mm_malloc and mm_free.
- *
- * NOTE TO STUDENTS: Replace this header comment with your own header
- * comment that gives a high level description of your solution.
- */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
@@ -18,10 +8,6 @@
 #include "mm.h"
 #include "memlib.h"
 
-/*********************************************************
- * NOTE TO STUDENTS: Before you do anything else, please
- * provide your team information in the following struct.
- ********************************************************/
 team_t team = {
     /* Team name */
     "7team",
